34bc0949ff37dfe8927f75f16928d71d010b87d1
J. Andrew Brassington <jabbrass@zoho.com>
2014-03-27 11:01:14 -0500

    Added Another

---
+++ GitHubTest/Controllers/Default1Controller3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubTest.Controllers
+{
+    public class Default1Controller3 : Controller
+    {
+        // GET: Default1Controller3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
