54101cd4d52ee008f16762d8dce43a81fade51b2
J. Andrew Brassington <jabbrass@zoho.com>
2014-03-27 10:59:48 -0500

    Added another default controller

---
+++ GitHubTest/Controllers/Default1Controller2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GitHubTest.Controllers
+{
+    public class Default1Controller2 : Controller
+    {
+        // GET: Default1Controller2
+        public ActionResult Index()
+        {
+            //Added Comment
+            return View();
+        }
+    }
+}
